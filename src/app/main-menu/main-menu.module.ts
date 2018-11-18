f14fb954783470f9a1f732708f832b01c4be9e65
Edson Feitosa <edsonfeitosa@MacBookGameiro.local>
2018-11-18 00:41:02 -0200

    addRouterMenu

--- src/app/main-menu/main-menu.module.ts
+++ src/app/main-menu/main-menu.module.ts
@@ -7,6 +7,14 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { Routes , RouterModule} from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+
+const routes: Routes = [
+  //    { path: '', component: AppComponent},
+    //  {path: 'usuario', loadChildren: './usuario/usuario-module/usuario-module.module#UsuarioModuleModule'},
+    //  { path: 'login', component: LoginComponent}
+  ];
 
 @NgModule({
   declarations: [
@@ -17,7 +25,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(), 
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(), 
+    RouterModule.forChild(routes)
   ],
   exports: [
     MainMenuComponent
